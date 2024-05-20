7c158522077a354b2e4428deab55714fe80a6260
Gabriel Mesquita <mesquita.gf@gmail.com>
2024-05-20 19:51:19 +0100

    first commit

---
+++ Code/src/main/java/com/api/Magic/MagicApplication.java
@@ -0,0 +1,13 @@
+package com.api.Magic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicApplication.class, args);
+	}
+
+}
